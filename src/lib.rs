c4216d192ba15cf0acc8828cb569f4d58d3b0fbd
Felipe Sere <github@felipesere.com>
2023-07-23 12:15:11 +0100

    Typed where-clause!

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use std::collections::VecDeque;
 use std::panic;
+use std::sync::Arc;
 
+use serde::de::DeserializeOwned;
 use serde_yaml::Value;
 
 #[derive(Clone, Debug)]
@@ -19,11 +21,23 @@ impl Query {
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 enum Step {
     Field(String),
     Index(usize),
     All,
+    Where(String, Arc<dyn Fn(&serde_yaml::Value) -> bool>),
+}
+
+impl std::fmt::Debug for Step {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Field(arg0) => f.debug_tuple("Field").field(arg0).finish(),
+            Self::Index(arg0) => f.debug_tuple("Index").field(arg0).finish(),
+            Self::All => write!(f, "All"),
+            Self::Where(arg0, _arg1) => f.debug_tuple("Where").field(arg0).finish(),
+        }
+    }
 }
 
 struct Paths<'input> {
@@ -91,6 +105,25 @@ fn dive(path: Paths<'_>) -> DiveOutcome<'_> {
                     query: remaining_query.clone(),
                 });
             }
+            DiveOutcome::Branch(additional_paths)
+        }
+        (Step::Where(field, predicate), Value::Sequence(s)) => {
+            let accessor = Value::String(field);
+            let mut additional_paths = Vec::new();
+            for val in s {
+                let Some(next_value) = val.get(accessor.clone()) else {
+                    continue;
+                };
+                if !predicate(next_value) {
+                    continue;
+                }
+
+                additional_paths.push(Paths {
+                    starting_point: val,
+                    query: remaining_query.clone(),
+                })
+            }
+
             DiveOutcome::Branch(additional_paths)
         }
         (step, value) => {
@@ -167,4 +200,69 @@ mod tests {
         let yoga: Vec<_> = navigate_iter(&yaml, yoga).collect();
         assert_eq!(yoga, vec![Value::String("yoga".to_string())]);
     }
+
+    #[test]
+    fn where_clause() {
+        let raw = indoc! {r#"
+            people:
+                - name: Felipe
+                  surname: Sere
+                  age: 32
+                  address:
+                    street: Foo
+                    postcode: 12345
+                    city: Legoland
+                  hobbies:
+                    - tennis
+                    - computer
+                - name: Charlotte
+                  surname: Fereday
+                  age: 31
+                  address:
+                    street: Bar
+                    postcode: 12345
+                    city: Legoland
+                  sports:
+                   - swimming
+                   - yoga
+            "#};
+        let yaml: Value = serde_yaml::from_str(raw).unwrap();
+
+        // TODO: A macro to do query!["people", 0, "name"] would be ace!
+        let names_of_people_aged_over_31 = Query {
+            steps: vec![
+                Step::Field("people".to_string()),
+                typed_where("age", |age: u32| age > 30),
+                Step::Field("name".to_string()),
+            ],
+        };
+
+        let felipe = navigate_iter(&yaml, names_of_people_aged_over_31)
+            .next()
+            .unwrap();
+        assert_eq!(felipe, Value::String("Felipe".into()));
+    }
 }
+
+fn typed_where<D: DeserializeOwned, F: Fn(D) -> bool + 'static>(
+    field: impl Into<String>,
+    fun: F,
+) -> Step {
+    Step::Where(
+        field.into(),
+        Arc::new(move |value: &serde_yaml::Value| {
+            let Ok(actual) = serde_yaml::from_value(value.clone()) else {
+                return false;
+            };
+            fun(actual)
+        }),
+    )
+}
+
+trait Where {
+    fn check(&self, value: &serde_yaml::Value) -> bool;
+}
+
+// Step 1: Have a trait that accepts a serde_yaml::Value
+// Step 2: Implement that trait on generic functions
+//
